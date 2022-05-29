2bab16505b515b1a28f8e92c1e704a321d63fc2c
paulinacarolina <codelikeamexican@gmail.com>
2022-05-29 15:31:27 -0500

    Clase 5:  Code First Creacion de Modelos

--- Platzi_Dotnet/Data/ApplicationDbContext.cs
+++ Platzi_Dotnet/Data/ApplicationDbContext.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+<<<<<<< HEAD
+using Platzi_Dotnet.Models;
+=======
+>>>>>>> b4fb2cf (Clase 4: Integracion de Entity Framework)
 
 namespace WebApplication1.Data
 {
@@ -7,6 +11,10 @@ namespace WebApplication1.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {       
         }
+<<<<<<< HEAD
+        public DbSet<Product> Product { get; set; }
+=======
 
+>>>>>>> b4fb2cf (Clase 4: Integracion de Entity Framework)
     }
 }
---
+++ Platzi_Dotnet/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Platzi_Dotnet.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(100)]
+        public string Name { get; set; }
+        
+        public double Price { get; set; }
+        
+        [StringLength(500)]
+        public string Description { get; set; }
+
+    }
+}
+
