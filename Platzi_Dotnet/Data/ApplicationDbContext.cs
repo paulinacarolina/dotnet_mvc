b4fb2cfb0c5de026acfe5ecf3c2462f87d57a1ce
paulinacarolina <codelikeamexican@gmail.com>
2022-05-29 11:51:30 -0500

    Clase 4: Integracion de Entity Framework

---
+++ Platzi_Dotnet/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {       
+        }
+
+    }
+}
