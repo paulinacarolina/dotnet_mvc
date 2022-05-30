57509f8ef96389e4f22f4cd30cc78cdf91591645
paulinacarolina <codelikeamexican@gmail.com>
2022-05-30 12:09:52 -0500

    Clase 9: Asertando Including

--- Platzi_Dotnet/XUnit.Coverlet.MSBuild/UnitTest1.cs
+++ Platzi_Dotnet/XUnit.Coverlet.MSBuild/UnitTest1.cs
@@ -5,12 +5,20 @@ namespace XUnit.Coverlet.MSBuild;
 public class UnitTest1
 {
     double numero = 3.0;
+    string[] nombres = { "Paulina", "Arantxa", "David" };
 
     [Fact]
     public void TestNumber()
     {
         Assert.IsType<int>(ParseDoubleToInt(numero));
     }
+
+    [Fact]
+    public void TestIncludingInCollection()
+    {
+        Assert.NotNull(nombres.Select(x=> x.ToString() == "Paulina"));
+    }
+
 
     #region funciones 
     public int ParseDoubleToInt(double numero)
