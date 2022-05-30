0f28a55de7d234dd39809bbe53395c5fb30ccfec
paulinacarolina <codelikeamexican@gmail.com>
2022-05-29 20:26:17 -0500

    Clase 9: Implementando Swagger

--- Platzi_Dotnet/Data/ApplicationDbContext.cs
+++ Platzi_Dotnet/Data/ApplicationDbContext.cs
@@ -1,20 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 using Platzi_Dotnet.Models;
-=======
->>>>>>> b4fb2cf (Clase 4: Integracion de Entity Framework)
 
-namespace WebApplication1.Data
+
+namespace Platzi_Dotnet.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {       
         }
-<<<<<<< HEAD
-        public DbSet<Product> Product { get; set; }
-=======
 
->>>>>>> b4fb2cf (Clase 4: Integracion de Entity Framework)
+        public DbSet<Product> Product { get; set; }
     }
 }
--- Platzi_Dotnet/Program.cs
+++ Platzi_Dotnet/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
+using Platzi_Dotnet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +19,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
