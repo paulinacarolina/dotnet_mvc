50a7f46d53dd6ee49b085382dcdb00136ef263f1
paulinacarolina <codelikeamexican@gmail.com>
2022-05-29 20:26:44 -0500

    Arreglar Migraciones

--- Platzi_Dotnet/Migrations/20220529195324_AddProduct.Designer.cs
+++ Platzi_Dotnet/Migrations/20220529195324_AddProduct.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using WebApplication1.Data;
+using Platzi_Dotnet.Data;
 
 #nullable disable
 
