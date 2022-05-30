2c3e4528396db0bcd47a5761756cc7762c9a2126
paulinacarolina <codelikeamexican@gmail.com>
2022-05-29 20:27:52 -0500

    Clase 11: Creacion de ViewModel y uso de AutoMapper

---
+++ Platzi_Dotnet/Utils/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Platzi_Dotnet.Models;
+using Platzi_Dotnet.VIewModels;
+
+namespace Platzi_Dotnet.Migrations
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ Platzi_Dotnet/VIewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Platzi_Dotnet.VIewModels
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+        
+    }
+}
